59c640b783172e3045c96dc8a85cc2218adf76f1
ubuntu <ubuntu@pm.me>
2021-03-16 15:30:45 +0000

    10 snaps

--- scripts/pingSnaps.sh
+++ scripts/pingSnaps.sh
@@ -2,11 +2,13 @@
 
 # start snap services
 pkill -9 -f snap.py
-ping -w2  10.41.0.150
-ping -w2  snap01.sas.pvt
-ping -w2  snap02.sas.pvt
-ping -w2  10.41.0.147
-ping -w2  10.41.0.137
-ping -w2  10.41.0.149
-ping -w2  10.41.0.249
-ping -w2  10.41.0.148
+ping -w2 10.41.0.150
+ping -w2 10.41.0.243
+ping -w2 10.41.0.246 
+ping -w2 10.41.0.147 
+ping -w2 10.41.0.137 
+ping -w2 10.41.0.149 
+ping -w2 10.41.0.249 
+ping -w2 10.41.0.148
+ping -w2 10.41.0.247
+ping -w2 10.41.0.248 
--- scripts/startSnaps.sh
+++ scripts/startSnaps.sh
@@ -10,3 +10,5 @@ pkill -9 -f snap.py
 ./snap.py -ef ../config/etcdConfig.yml -cf ../config/dsa.yaml -hs 10.41.0.149 -df ../config/beamformer_weights.yaml -n 6 &
 ./snap.py -ef ../config/etcdConfig.yml -cf ../config/dsa.yaml -hs 10.41.0.249 -df ../config/beamformer_weights.yaml -n 7 &
 ./snap.py -ef ../config/etcdConfig.yml -cf ../config/dsa.yaml -hs 10.41.0.148 -df ../config/beamformer_weights.yaml -n 8 &
+./snap.py -ef ../config/etcdConfig.yml -cf ../config/dsa.yaml -hs 10.41.0.247 -df ../config/beamformer_weights.yaml -n 9 &
+./snap.py -ef ../config/etcdConfig.yml -cf ../config/dsa.yaml -hs 10.41.0.248 -df ../config/beamformer_weights.yaml -n 10 &
